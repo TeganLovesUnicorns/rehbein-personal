28496352fe73b03da48c554cfdbe3874d89b3c79
flyingguitarman <flyingguitarman@gmail.com>
2017-08-13 19:17:18 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import HomeSlider from './HomeSlider.js';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Coming Soon...</h2>
+        <h3>danielrehbein.com</h3>
+        <div> {HomeSlider} </div>
+      </div> 
+    );
+  }
+}
+
+
+
+export default App;
