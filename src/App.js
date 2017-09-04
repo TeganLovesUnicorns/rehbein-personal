c15d6162cb1c015868071205fc564edc3f090391
flyingguitarman <flyingguitarman@gmail.com>
2017-09-03 23:46:33 -0500

    menu beginnings

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React, { Component } from 'react';
 import './App.css';
 import Footer from '../src/Footer.js';
 import HomeSlider from '../src/HomeSlider.js';
+import MenuBar from './MenuBar.js';
+
 
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className='container' >
+        <MenuBar />
         <HomeSlider />
         <Footer />
       </div>
--- src/MenuBar.js
+++ src/MenuBar.js
@@ -1,1 +1,42 @@
 // Top Menu for site. Drop Down.
+
+import React, { Component } from 'react';
+
+class MenuBar extends Component{
+    constructor (props) {
+        super(props);
+        this.state = {
+            selectedPage : 'Home'
+        };
+
+        this.updatePage = this.updatePage.bind(this);
+    }
+
+    updatePage(page) {
+        this.setState(function(){
+            return{
+                selectedPage: page
+            }
+        });
+    }
+
+    render() {
+        var pages = ['Home', 'Lighting', 'Photography', 'Graphics'];
+        return (
+            <ul className='pages'>
+                {pages.map(function (page) {
+                    return(
+                        <li
+                        style={page === this.state.selectedPage ? {color:'#d0021b'}: null}
+                        onClick={this.updatePage.bind(null, page)}
+                        key={page}>
+                        {page}
+                        </li>
+                    )
+                }, this)}
+            </ul>
+        )
+    }
+};
+
+export default MenuBar;
